45a2d421db2648a97a89c35bf0c02073fb14da16
choemingyu <mchead1@naver.com>
2022-07-12 20:30:36 +0900

    :hammer: 프로젝트 기본 구조 세팅

---
+++ app/exceptions/duplication_error.py
@@ -0,0 +1,8 @@
+from pymysql.err import IntegrityError
+
+
+class DuplicationError(Exception):
+    @staticmethod
+    def is_duplication_err(e: IntegrityError):
+        code = e.orig.args[0]
+        return code == 1062
