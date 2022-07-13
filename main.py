03bb20d6f79b715c3f11320cdebef93f7faecbe7
choemingyu <mchead1@naver.com>
2022-07-13 17:00:59 +0900

    :fire: 불필요한 테스트용 초기 API 제거

--- main.py
+++ main.py
@@ -4,15 +4,4 @@ from app.apis.v1.api import v1_router
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
-
-
 app.include_router(v1_router, tags=["v1"], prefix="/api/v1")
