7479fb112078219bb405529018b8672027dca3da
choemingyu <mchead1@naver.com>
2022-07-13 18:48:01 +0900

    :wrench: 댓글 테이블 마이그레이션

---
+++ alembic/versions/95a1c258206f_create_reply_table.py
@@ -0,0 +1,35 @@
+"""create reply table
+
+Revision ID: 95a1c258206f
+Revises: aa2d20b6a248
+Create Date: 2022-07-13 18:41:37.393078
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '95a1c258206f'
+down_revision = 'aa2d20b6a248'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "replies",
+        sa.Column("id", sa.Integer, primary_key=True),
+        sa.Column("feed_id", sa.Integer, nullable=False),
+        sa.Column("nickname", sa.String(50), nullable=False),
+        sa.Column("comment", sa.String(255), nullable=False),
+        sa.Column("created_at", sa.DateTime, server_default=sa.func.current_timestamp()),
+        sa.Column("deleted_at", sa.DateTime),
+    )
+
+    op.create_index(op.f('index_replies_nickname'), 'replies', ['nickname'], unique=False)
+    op.create_index(op.f('index_replies_feed_id'), 'replies', ['feed_id'], unique=False)
+
+
+def downgrade() -> None:
+    op.drop_table("feeds")
